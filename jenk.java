ace8071c35d9c2c63bf34d09ec0e4100dcfc19f2
M Nikhil <155064830+NikhilMNSo@users.noreply.github.com>
2024-04-22 13:50:33 +0530

    Add files via upload

---
+++ jenk.java
@@ -0,0 +1,8 @@
+public class jenk
+{
+    public static void main(String[] args) {
+        System.out.println("M NIKHIL");
+        System.out.println("21MIC0124");
+        System.out.println("I am from Vit");
+    }
+}
